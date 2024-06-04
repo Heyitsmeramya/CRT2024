756253d1af7c6bbda6f6ca64ee97a5d6da46944e
Heyitsmeramya <125736641+Heyitsmeramya@users.noreply.github.com>
2024-06-04 10:01:17 +0530

    Create sum of two no.s

---
+++ sum of two no.s
@@ -0,0 +1,12 @@
+sum of two numbers
+#include <stdio.h>
+•	int main()
+•	{
+•	    int a,b;
+•	    printf(" enter two numbers");
+•	    scanf("%d %d",&a,&b);
+•	    int r=a+b;
+•	    printf("%d",r);
+•	
+•	    return 0;
+•	}
